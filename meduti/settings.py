802ef58ce5b4e2873ef0c69cd246dfcdd17e9731
Nando-Xavier <fernando.xavierferreira@gmail.com>
2019-11-02 12:34:15 -0300

    corrigindo settings

--- meduti/settings.py
+++ meduti/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'meduti.wsgi.application'
 default_dburl = 'sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
-    'default': config('DATABASE_URL', default = default_dburl, cast = db_url),
+    'default': config('DATABASE_URL', default = default_dburl, cast = dburl),
 
 
 }
